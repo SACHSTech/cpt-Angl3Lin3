fe41dd1faf8076f196a84568f994f5dc65ee7407
Angl3Lin3 <angle.lin23@ycsdbk12.ca>
2023-01-10 10:46:40 -0500

    Created ArrayList in DataBase

--- src/cpt/DataBase.java
+++ src/cpt/DataBase.java
@@ -1,8 +1,11 @@
 package cpt;
+import java.util.ArrayList;
 
 public class DataBase {
-    
+
     public static void database(String[] args) {
         
+        ArrayList<Data> data = new ArrayList<Data>();
+
     }
 }
