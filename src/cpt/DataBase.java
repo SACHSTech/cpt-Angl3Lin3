f8ab96e8c526a9697a9b96d3426f31e33e64a324
Angl3Lin3 <angle.lin23@ycsdbk12.ca>
2023-01-11 10:06:43 -0500

    Added FileReader and put values in ArrayList

--- src/cpt/DataBase.java
+++ src/cpt/DataBase.java
@@ -1,8 +1,43 @@
 package cpt;
+import java.io.FileReader;
 import java.util.ArrayList;
+import java.io.*;
 
 public class DataBase {
 
-    ArrayList<Data> data = new ArrayList<Data>();
+    public static void main(String[] args) {
+        
+        try {
+            BufferedReader scan = new BufferedReader(new FileReader("src/cpt/ValorantRanks.csv"));
+        ArrayList<Data> data = new ArrayList<Data>();
+
+        String line = null;
+        int j = 0;
+
+        while((line = scan.readLine()) != null){
+            String[] str = line.split(",");
+
+            if(j != 0) {
+                for(int i = 0; i < str.length; i++) {
+                    System.out.println(str[i]);
+                    double percent = Double.parseDouble(str[1]);
+                    Data input = new Data(str[0], percent, str[2]);
+                    data.add(input);
+                }
+            }
+            j++;
+            
+        }
+
+        System.out.println(data);
+        
+        } catch(Exception e) {
+            
+        }
+
+        
+
+    }
+   
 
 }
